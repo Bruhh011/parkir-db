eb9872ced9fde63ccf02b166df1714170f5d7837
Bruhh011 <arifky.danihs@gmail.com>
2025-10-22 20:21:13 +0700

    memastikan semua petugas punya jabatan

--- menjabat.py
+++ menjabat.py
@@ -9,11 +9,20 @@ random.seed(1000)
 
 conn, cur = db_con()
 
-for i in range(10):
-    cur.execute("SELECT id_petugas FROM petugas ORDER BY RANDOM() LIMIT 1;")
-    rand_petugas = cur.fetchone()[0]
-    cur.execute("SELECT id_jabatan FROM jabatan ORDER BY RANDOM() LIMIT 1;")
-    rand_jabatan = cur.fetchone()[0]
-    cur.execute("INSERT INTO menjabat(id_petugas, id_jabatan) VALUES (%s, %s) ON CONFLICT DO NOTHING", (rand_petugas, rand_jabatan,))
+menjabat = [
+        (0, 9),
+        (1, 8),
+        (2, 7),
+        (3, 6),
+        (4, 5),
+        (5, 4),
+        (6, 3),
+        (7, 2),
+        (8, 1),
+        (9, 0)
+        ]
+
+for i, j in menjabat:
+    cur.execute("INSERT INTO menjabat(id_petugas, id_jabatan) VALUES (%s, %s) ON CONFLICT DO NOTHING", (i, j,))
 
 db_close(conn, cur)
