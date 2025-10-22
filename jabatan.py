0cce8ae50484ce31d823f1ada2c4855b1b6861f3
Bruhh011 <arifky.danihs@gmail.com>
2025-10-22 20:20:39 +0700

    template sesuai dengan sebelumnya (diganti dari jabatanXY dan dari lorem ipsum)

--- jabatan.py
+++ jabatan.py
@@ -9,11 +9,20 @@ random.seed(1000)
 
 conn, cur = db_con()
 
-hak_akses = "Lorem ipsum dolor sit amet"
+jabatan = [
+        (0, "direktur utama", "akses penuh ke seluruh sistem", 15000000),
+        (1, "manajer operasional", "akses ke laporan dan data operasional", 10000000),
+        (2, "supervisor lapangan", "akses data parkir harian", 5500000),
+        (3, "admin sistem", "akses konfigurasi sistem dan pengguna", 6000000),
+        (4, "petugas parkir1", "akses input dan pembayaran kendaraan keluar", 3500000),
+        (5, "petugas parkir2", "akses input dan pembayaran keluar", 3500000),
+        (6, "keamanan", "akses laporan keamanan area parkir", 4000000),
+        (7, "teknisi sistem", "akses pemeliharaan dan sistem", 5500000),
+        (8, "customer service", "akses data pelanggan dan komplain", 5000000),
+        (9, "kepala shift", "akses data shift dan laporan harian", 6500000)
+        ]
 
-for i in range(10):
-    nama_jabatan = str("jabatan" + rstr.xeger(r'[0-1][0-9]'))
-    gaji_pokok = rstr.xeger(r'[3-5][0-9]00000')
-    cur.execute("INSERT INTO jabatan(id_jabatan, nama_jabatan, hak_akses, gaji_Pokok) VALUES (%s, %s, %s, %s) ON CONFLICT DO NOTHING", (i, nama_jabatan, hak_akses, gaji_pokok,))
+for i, j, k, l in jabatan:
+    cur.execute("INSERT INTO jabatan(id_jabatan, nama_jabatan, hak_akses, gaji_Pokok) VALUES (%s, %s, %s, %s) ON CONFLICT DO NOTHING", (i, j, k, l,))
 
 db_close(conn, cur)
