b3e2df43aab8bd54e8b3f2c05c299a5edf44d8e2
Bruhh011 <arifky.danihs@gmail.com>
2025-10-22 13:06:56 +0700

    biar kendaraan jadi 10000

--- kendaraan.py
+++ kendaraan.py
@@ -10,15 +10,34 @@ random.seed(1000)
 conn, cur = db_con()
 
 plat_lst = ["A", "B", "D", "E", "F", "G", "H", "K", "L", "M", "N", "S", "T", "AA", "AB", "AD"]
-
 jenis = ["roda 2", "roda 4"]
 
-for i in range(10000):
-    plat_awal = "".join(random.choices(plat_lst))
+target_count = 10000
+inserted = 0
+seen = set()  
+
+while inserted < target_count:
+    
+    plat_awal = random.choice(plat_lst)
     dummy_data = rstr.xeger(r'[1-9]{4} [A-Z]{1,2}')
-    plat_nomor = str(plat_awal + " " + dummy_data )
-    jenis_kendaraan  = "".join(random.choices(jenis))
-    cur.execute("INSERT INTO kendaraan(plat_nomor, jenis_kendaraan) VALUES (%s, %s) ON CONFLICT DO NOTHING", (plat_nomor, jenis_kendaraan,))
+    plat_nomor = f"{plat_awal} {dummy_data}"
+    jenis_kendaraan = random.choice(jenis)
+
+    
+    if plat_nomor in seen:
+        continue
+
+    try:
+        cur.execute(
+            "INSERT INTO kendaraan(plat_nomor, jenis_kendaraan) VALUES (%s, %s) ON CONFLICT DO NOTHING",
+            (plat_nomor, jenis_kendaraan)
+        )
+        if cur.rowcount > 0:  
+            seen.add(plat_nomor)
+            inserted += 1
+    except Exception as e:
+        conn.rollback()
 
 
 db_close(conn, cur)
+
